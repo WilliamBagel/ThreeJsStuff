5cefeb1a53a9d32e992994308d497e81fd8d42f6
WilliamBagel <102871850+WilliamBagel@users.noreply.github.com>
2022-04-02 13:23:25 -0400

    Update GLTFLoader.module.js

--- GLTFLoader.module.js
+++ GLTFLoader.module.js
@@ -61,7 +61,7 @@ import {
 	Vector3,
 	VectorKeyframeTrack,
 	sRGBEncoding
-} from 'https://threejs.org/build/three.js';
+} from 'https://threejs.org/build/three.module.js';
 
 class GLTFLoader extends Loader {
 
